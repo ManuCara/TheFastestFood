a4ebca4df769b2853fc0c9a5726236e0aa10e98c
ManuCara <79227421+ManuCara@users.noreply.github.com>
2023-10-25 22:19:25 +0200

    Stabilizzazione

--- Codice/Main.java
+++ Codice/Main.java
@@ -4,13 +4,13 @@ public class Main {
 
     public static void main(String[] args){
       
-       System.out.println("Hello World!");
 
       Scanner reader = new Scanner(System.in);
       System.out.print("Enter a number: ");
       int number = reader.nextInt();
       System.out.println("You entered: " + number);
   
+       System.out.println("Hello World!");
 
        
     }
