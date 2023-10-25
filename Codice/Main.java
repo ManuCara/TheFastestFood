0215099d58433468e75035a06dea61430bc32b25
ManuCara <79227421+ManuCara@users.noreply.github.com>
2023-10-25 16:35:10 +0200

    Update Main.java

--- Codice/Main.java
+++ Codice/Main.java
@@ -3,7 +3,15 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args){
-      System.out.println("Hello World!");
+      
+       System.out.println("Hello World!");
+
+      Scanner reader = new Scanner(System.in);
+      System.out.print("Enter a number: ");
+      int number = reader.nextInt();
+      System.out.println("You entered: " + number);
+  
+
        
     }
 }
