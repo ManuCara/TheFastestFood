117d784705551ec3d8dcfba09d894dd05de81cb0
ManuCara <79227421+ManuCara@users.noreply.github.com>
2023-10-27 01:24:16 +0200

    Prova di Caricamento Sfondo

---
+++ Codice/CaricatoreImmagini.java
@@ -0,0 +1,27 @@
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.imageio.ImageIO;
+
+public class CaricatoreImmagini {
+  
+  BufferedImage image = null;
+
+  public CaricatoreImmagini() {
+  }
+
+  public BufferedImage caricaImmagine(String posizione){
+    try{
+      image = ImageIO.read(getClass().getResource(posizione));
+            System.out.println("C'è l'immagine!");
+
+      
+    }catch(IOException ex){
+      System.out.println("Indirizzo sbagliato!");
+     // Logger.getLogger(CaricatoreImmagini.class.getName()).log(Level.SEVERE, ex, "Indirizzo specificato sbagliato!");
+    }
+    return image;
+  }
+}
--- Codice/Main.java
+++ Codice/Main.java
@@ -1,17 +1,42 @@
 import java.util.*;
+import java.awt.Canvas;
+import java.awt.Dimension;
+import java.awt.image.BufferedImage;
 
-public class Main {
+import javax.swing.JFrame;
+
+public class Main extends Canvas{
+  private static final int larghezza = 1980;
+  private static final int altezza = 1080;
+  private static final String nomeGioco = "The Fastest Food";
+  BufferedImage sfondo = null;
+
+  public Main(){
+    caricaRisorse();
+  }
 
     public static void main(String[] args){
+      Main gioco = new Main();
+
+      JFrame finestraGioco = new JFrame(nomeGioco, null);
       
+      Dimension dimensioneFinestra = new Dimension(larghezza,altezza);
+      finestraGioco.setPreferredSize(dimensioneFinestra);
+      finestraGioco.setMaximumSize(dimensioneFinestra);
+      finestraGioco.setResizable(false);
 
-      Scanner reader = new Scanner(System.in);
-      System.out.print("Enter a number: ");
-      int number = reader.nextInt();
-      System.out.println("You entered: " + number);
-  
-       System.out.println("Hello World!");
+      finestraGioco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-       
+      finestraGioco.add(gioco);
+      
+      finestraGioco.pack();
+      finestraGioco.setVisible(true);
     }
+
+    private void caricaRisorse(){
+      CaricatoreImmagini loader = new CaricatoreImmagini();
+      sfondo = loader.caricaImmagine("/Immagini/Sfondi/Prova.jpg");
+      System.out.println("Sfondo caricato correttamente!");
+    }
+
 }
